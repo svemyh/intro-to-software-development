3b061d0021aa42c8302cf13055e742cfb2d4f176
svemyh <s-myhre@outlook.com>
2025-06-30 14:41:31 +0200

    Add gardners equation

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -7,3 +7,10 @@ def test_length_of_string() -> None:
 def test_find_average_of_list_of_numbers() -> None:
     test_list = [1, 2, 3, 4, 5, 6]
     assert find_average(test_list) == 3.5
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
