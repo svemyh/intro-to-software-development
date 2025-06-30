b88af80242b1fd36405ebb85b629f25a578a9d2b
svemyh <s-myhre@outlook.com>
2025-06-30 14:59:12 +0200

    refactor: black

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def find_average(list_of_numbers: list[int]) -> float:
     """Calculate the average of some numbers
     Args:
@@ -10,13 +11,16 @@ def find_average(list_of_numbers: list[int]) -> float:
     avg = np.average(list_of_numbers)
     return float(avg)
 
+
 GARDNERS_ALPHA = 0.31
 GARDNERS_BETA = 0.25
 
+
 def gardners_equation(velocity: float) -> float:
     """Calculate density from velocity using Gardner's equation"""
-    density = GARDNERS_ALPHA * velocity ** GARDNERS_BETA
+    density = GARDNERS_ALPHA * velocity**GARDNERS_BETA
     return density
+
 
 def inverse_gardners_equation(density: float) -> float:
     """Calculate velocity from density using Gardner's equation"""
