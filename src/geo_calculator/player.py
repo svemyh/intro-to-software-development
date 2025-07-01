6632c589551b6bf22b28d3bf3e2fd441430f49d9
svemyh <s-myhre@outlook.com>
2025-07-01 10:22:55 +0200

    add Player

---
+++ src/geo_calculator/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self):
+        self.name = "Mick Jagger"
+
+    def getName(self):
+        return self.name
---
+++ tests/rolling_stone/test_player.py
@@ -0,0 +1,6 @@
+from geo_calculator.player import Player
+
+
+def test_player():
+    player = Player()
+    assert isinstance(player, Player)
