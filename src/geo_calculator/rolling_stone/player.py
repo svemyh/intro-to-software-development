413f65127821e347c5a74bbd8ca50f31892fe851
svemyh <s-myhre@outlook.com>
2025-07-01 10:38:38 +0200

    add score player param

--- src/geo_calculator/rolling_stone/player.py
+++ src/geo_calculator/rolling_stone/player.py
@@ -1,6 +1,10 @@
 class Player:
     def __init__(self):
         self.name = "Mick Jagger"
+        self.score = 0
 
     def getName(self):
         return self.name
+
+    def receive_score(self, score):
+        self.score += score
--- tests/rolling_stone/test_player.py
+++ tests/rolling_stone/test_player.py
@@ -1,6 +1,17 @@
-from geo_calculator.player import Player
+from geo_calculator.rolling_stone.player import Player
 
 
 def test_player():
     player = Player()
     assert isinstance(player, Player)
+
+
+def test_player_receive_score():
+    # Arrange
+    player = Player()
+    RECEIVED_SCORE = 10
+    assert player.score == 0
+    # Act
+    player.receive_score(RECEIVED_SCORE)
+    # Assert
+    assert player.score == RECEIVED_SCORE
