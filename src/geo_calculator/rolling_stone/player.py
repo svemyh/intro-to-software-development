ccd09af9a7d7703b5870c6565dfa270ff2956cfe
svemyh <s-myhre@outlook.com>
2025-07-01 12:40:24 +0200

    add mock test

--- src/geo_calculator/rolling_stone/player.py
+++ src/geo_calculator/rolling_stone/player.py
@@ -1,6 +1,10 @@
+import re
+from geo_calculator.rolling_stone.exceptions import InvalidPlayerNameException
+
+
 class Player:
     def __init__(self):
-        self.name = "Mick Jagger"
+        self.name = None
         self.score = 0
 
     def getName(self):
@@ -8,3 +12,24 @@ class Player:
 
     def receive_score(self, score):
         self.score += score
+
+    def _get_name_for_player_from_input(self):
+        return input("Please enter your name: ")
+
+    def prompt_for_name(self):
+        name = self._get_name_for_player_from_input()
+
+        if not name:
+            raise InvalidPlayerNameException("Name must have at least one character")
+
+        if not re.match(r"^[a-zA-Z ]+$", name):
+            raise InvalidPlayerNameException(
+                "Name can only contain alphabetic characters and spaces"
+            )
+
+        if "  " in name:
+            raise InvalidPlayerNameException(
+                "Name cannot have multiple spaces in a row"
+            )
+
+        self.name = name
--- tests/rolling_stone/test_player.py
+++ tests/rolling_stone/test_player.py
@@ -1,5 +1,6 @@
 import pytest
 from geo_calculator.rolling_stone.player import Player
+from geo_calculator.rolling_stone.exceptions import InvalidPlayerNameException
 
 
 @pytest.fixture
@@ -19,3 +20,38 @@ def test_player_receive_score(new_player):
     new_player.receive_score(RECEIVED_SCORE)
     # Assert
     assert new_player.score == RECEIVED_SCORE
+
+
+def test_input_name_for_player(mocker, new_player):
+    # Arrange
+    TEST_NAME = "Ola Nordmann"
+    mocker.patch.object(
+        Player, "_get_name_for_player_from_input", return_value=TEST_NAME
+    )
+    assert new_player.name is None
+
+    # Act
+    new_player.prompt_for_name()
+
+    # Assert
+    assert new_player.name == TEST_NAME
+
+
+@pytest.mark.parametrize(
+    "invalid_name, exception_match_string",
+    [
+        ("", "Name must have at least one character"),
+        ("123456789", "Name can only contain alphabetic characters and spaces"),
+        ("NameWith@Symbol", "Name can only contain alphabetic characters and spaces"),
+        ("Name With Too  Many   Spaces", "Name cannot have multiple spaces in a row"),
+    ],
+)
+def test_input_invalid_name_for_player(
+    mocker, new_player, invalid_name, exception_match_string
+):
+    mocker.patch.object(
+        Player, "_get_name_for_player_from_input", return_value=invalid_name
+    )
+    with pytest.raises(InvalidPlayerNameException, match=exception_match_string):
+        new_player.prompt_for_name()
+    assert new_player.name is None
